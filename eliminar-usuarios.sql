90f3af257bc13e92b5886aa604e87153650b2e2e
Yoelys Figueredo Padrón <yoelys1982@gmail.com>
2022-12-02 00:24:02 -0300

    Subiendo backup de estructura y de datos de las tablas

--- eliminar-usuarios.sql
+++
@@ -1,9 +0,0 @@
-DROP USER 'yoelys_dev1'@'localhost',
-		  'yoelys_dev2'@'localhost',
-		  'yoelys_dev3'@'localhost',
-		  'yoelys_readonly1'@'localhost',
-		  'yoelys_readonly2'@'localhost',
-		  'yoelys_readonly3'@'localhost',
-		  'yoelys_readwrite1'@'localhost',
-		  'yoelys_readwrite2'@'localhost',
-		  'yoelys_readwrite3'@'localhost';
