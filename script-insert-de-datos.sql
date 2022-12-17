b4bea981cd47d400b0f2b523fcad1a4eef3558f8
Yoelys Figueredo Padrón <yoelys1982@gmail.com>
2022-12-16 23:46:19 -0300

    Actualizando script de inserción

--- script-insert-de-datos.sql
+++ script-insert-de-datos.sql
@@ -11,18 +11,32 @@ VALUES
     (null, '55221990','Jennifer','Herrera','F','Soltera','19940210','55569988','jennifer@gmail.com','Caballito'),
     (null, '13244990','Susana','Lopez','M','Soltera','19950606','99887654','susana@gmail.com','Villa Crespo'),
     (null, '56271990','Hector','Jimenez','M','Soltero','19960107','97654321','hector@gmail.com','Colegiales'),
-    (null, '33211990','Pedro','Fernandez','M','Casado','19931224','23234141','pedro@gmail.com','Villa Urquiza');
+    (null, '33211990','Pedro','Fernandez','M','Casado','19931224','23234141','pedro@gmail.com','Villa Urquiza'),
+	(null, '78722447','Sara','Rovira','F','Soltera','19571111','01259161','sara.rovira@gmail.com','Barrio Parque'),
+    (null, '33741588','Gabriela','Jara','F','Divorciada','19760409','55809183','gjara@gmail.com','Parque Chas'),
+    (null, '35939299','Yassine','Paz','M','Viudo','19500212','20499693','yassine.paz@gmail.com','Almagro'),
+    (null, '94433274','Consuelo','Ares','F','Soltera','19970902','94104096','consuelitoshine@gmail.com','Palermo'),
+    (null, '76696865','Rogelio','Badia','M','Casado','19740406','74482057','soybadia@gmail.com','Belgrano'),
+    (null, '86546166','Laura','Castro','F','Casada','19800524','25603569','lauri@gmail.com','Hurlingham'),
+    (null, '38889527','Alexia','Miralles','F','Viuda','19590412','05216969','a.miralles@gmail.com','Sarandi'),
+    (null, '59221748','Alma','Gonzalez','F','Soltera','19890520','16475971','almita@gmail.com','Villa Crespo'),
+    (null, '38170900','Patricio','Castro','M','Soltero','19940518','93152999','patooo@gmail.com','Villa Lugano'),
+    (null, '89480616','Roberto','Roca','M','Casado','19851210','95628498','robert@gmail.com','Monte Castro');
     
 INSERT INTO carreras
 VALUES
-	('AA-001', 'Ingenieria en Sistemas', 200000),
-    ('AA-002', 'Licenciatura en Administracion de Empresas', 180000),
+	('AA-001', 'Ingeniería en Sistemas', 200000),
+    ('AA-002', 'Licenciatura en Administración de Empresas', 180000),
     ('AA-003', 'Licenciatura en Derecho', 220000),
-    ('AA-004', 'Licenciatura en Mercadotecnia', 190000),
-    ('AA-005', 'Licenciatura en Psicologia', 250000),
-    ('AA-006', 'Licenciatura en Teologia', 170000),
-    ('AA-007', 'Licenciatura en Informatica', 190000),
-    ('AA-008', 'Laboratorio Clinico', 175000);
+    ('AA-004', 'Licenciatura en Marketing', 190000),
+    ('AA-005', 'Licenciatura en Psicología', 250000),
+    ('AA-006', 'Licenciatura en Teología', 170000),
+    ('AA-007', 'Licenciatura en Informática', 190000),
+    ('AA-008', 'Técnico en Laboratorio Clínico', 175000),
+	('AA-009', 'Licenciatura en Crimonología', 350000),
+    ('AA-010', 'Doctorado en Derecho Penal', 900000),
+    ('AA-011', 'Master en Marketing de Negocios', 890000),
+    ('AA-012', 'Licenciatura en Relaciones Públicas', 475000);
 
 INSERT INTO periodos_academicos
 VALUES
@@ -38,11 +52,16 @@ VALUES
     
 INSERT INTO profesiones
 VALUES 
-	('MGT', 'Master en Gerstion de Tecnologias'),
+	('MGT', 'Master en Gestión de Tecnologías'),
 	('LCI', 'Lic. en Comercio Internacional'),
     ('IES', 'Ingeniero En Sistemas'),
-    ('LPS', 'Lic. en Psicologia'),
-    ('LED', 'Lic. en Derecho');
+    ('LPS', 'Lic. en Psicología'),
+    ('LED', 'Lic. en Derecho'),
+	('LAE', 'Lic. en Administración de Empresas'),
+	('LSI', 'Lic. en Sistemas de Información'),
+    ('MMN', 'Master en Marketing de Negocios'),
+    ('DDP', 'Doctorado en Derecho Penal'),
+    ('LRP', 'Lic. en Relaciones Públicas');
 
 -- Asignaturas de Ingenieria
 INSERT INTO asignaturas
@@ -105,7 +124,12 @@ VALUES
     (null, 'AA-005', 6, 4, '20200710'),
     (null, 'AA-006', 6, 5, '20200620'),
     (null, 'AA-007', 8, 6, '20201120'),
-    (null, 'AA-007', 8, 10, '20201205');
+    (null, 'AA-009', 8, 10, '20201205'),
+	(null, 'AA-010', 2, 8, '20211105'),
+    (null, 'AA-002', 6, 4, '20220710'),
+    (null, 'AA-011', 1, 1, '20210429'),
+    (null, 'AA-012', 8, 10, '20191104'),
+    (null, 'AA-004', 3, 7, '20191024');
     
 INSERT INTO notas
 VALUES
@@ -168,7 +192,12 @@ VALUES
     (null, '15031985', 'Diana', 'Peralta', 'F', 'peralta.diana@gmail.com', '99989796', 'Soltera', 'LPS', '19850511', '20160120'),
     (null, '15031983', 'Maria', 'Funez', 'F', 'mariafunez1@gmail.com', '97989786', 'Casada', 'LPS', '19850818', '20160120'),
 	(null, '08011990', 'Victor', 'Acosta', 'M', 'acosvict@yahoo.com', '88989986', 'Soltero', 'LPS', '19900621', '20170912'),
-    (null, '01011968', 'Juan', 'Zavala', 'M', 'jzavala28@yahoo.com', '77889886', 'Casado', 'LED', '19680529', '20170903');
+    (null, '01011968', 'Juan', 'Zavala', 'M', 'jzavala28@yahoo.com', '77889886', 'Casado', 'LED', '19680529', '20170903'),
+	(null, '18024566', 'Margarita', 'Monteiro', 'F', 'marga.mont@gmail.com', '44323456', 'Casada', 'DDP', '19600527', '20171120'),
+    (null, '19762344', 'Marcos', 'Acosta', 'M', 'marcos.a@gmail.com', '49076522', 'Soltero', 'LSI', '19870819', '20201209'),
+    (null, '99855441', 'Luana', 'Britos', 'F', 'britosluana@gmail.com', '43217655', 'Casada', 'MMN', '19811020', '20180221'),
+	(null, '47983074', 'Dante', 'Castro', 'M', 'profedante@icloud.com', '45678833', 'Soltero', 'LAE', '19910430', '20210511'),
+    (null, '16523499', 'Gabriel', 'Juarez', 'M', 'juarez.gabriel80@ba.edu.ar', '46737129', 'Divorciado', 'LRP', '19770322', '20141201');
     
 INSERT INTO profesores_asignaturas
 VALUES
